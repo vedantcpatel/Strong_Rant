aac3d7e92f763761649760852bbb65e5624977e0
vedantcpatel <37755427+vedantcpatel@users.noreply.github.com>
2018-07-25 18:28:47 -0400

    Update Strong_Rant_SC.py

--- Strong_Rant_SC.py
+++ Strong_Rant_SC.py
@@ -1,6 +1,13 @@
+
 import random
 
-MATH = ('pre-algebra', 'algebra 1', 'algebra 2', 'geometry', 'trigonometry', 'statistics', 'probability', 'precalculus', 'calculus AB', 'calculus BC', 'differential equations', 'linear algebra', 'discrete mathematics')
-EZ = ('easy', 'uncomplicated', 'straightforward', 'effortless', 'simple', 'unchallenging', 'undemanding', 'elementary')
+math = ('pre-algebra', 'algebra 1', 'algebra 2', 'geometry', 'trigonometry', 'statistics', 'probability', 'precalculus', 'calculus AB', 'calculus BC', 'differential equations', 'linear algebra', 'discrete mathematics')
+easy = ('easy', 'uncomplicated', 'straightforward', 'effortless', 'simple', 'unchallenging', 'undemanding', 'elementary')
 
-print('That', (random.choice(MATH)), 'problem was so', (random.choice(EZ)), '!')
+num_of_iterations = int(input("How many times do you want to hear the joke? (please enter a natural number) "))
+
+#What an unnamed student said for a geometry problem (s)he answered. Later, (s)he realized (s)he was incorrect. :)
+for i in range(num_of_iterations):
+    print('That '+(random.choice(math))+' problem was so '+(random.choice(easy))+'!')
+
+input()
